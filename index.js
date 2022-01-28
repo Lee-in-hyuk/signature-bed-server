c6daf4a15baea766f3c8f1d86d2fa8b24bc9844c
hyuk <dlsgur0906@gmail.com>
2022-01-28 16:13:44 +0900

    Board페이지에 mysql데이터들 가지고 오는작업 완료

--- index.js
+++ index.js
@@ -5,16 +5,42 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
+const fs = require('fs');
+const dataj = fs.readFileSync("./database.json");
+const parseData = JSON.parse(dataj);    // database.json받아온거를 객체형태로 변수에 담음
+const mysql = require('mysql');
+
+// php에서 connect 만든거랑 같은 개념
+const connection = mysql.createConnection({
+    host:parseData.host,
+    user: parseData.user,
+    password: parseData.password,
+    port: parseData.port,
+    database: parseData.database
+})
+
 app.use(express.json()); //json형식의 데이터를 처리할 수 있도록 설정
 app.use(cors());        // 브라우저의 다양한 사용을 위해 설정
 
-// get요청이 오면 res.send를 보내주겠다.
-app.get('/board', async(req,res)=>{
-    res.send('상품정보 리스트 입니다.')
-})
+// // get요청이 오면 res.send를 보내주겠다.
+// app.get('/board', async(req,res)=>{
+//     res.send('상품정보 리스트 입니다.')
+// })
 // post요청 오면 res.send를 보내주겠다.
 app.post('/createboard', async(req,res)=>{
     res.send('등록되었습니다.')
+})
+
+app.get('/board', async(req,res)=>{
+    // php할 때 result = mysqli_query()날려주는거랑 같은 개념
+    // query(쿼리문, 콜백함수(에러값,결과,컬럼))
+    connection.query(
+        "SELECT * FROM board",
+        (err, rows, fields) =>{
+            res.send(rows);
+        }
+    )
+    // 여기까지 작성하고 http://localhost:8080/board 브라우저에 입력했을 때 데이터 잘 나오면 성공
 })
 
 // 셋팅한 app을 실행
